bd6ea1469cf2948e395c95028d5988f09d6bbf34
seungeun90 <94624666+seungeun90@users.noreply.github.com>
2024-07-18 13:51:13 +0900

    [level 3] Title: 베스트앨범, Time: 4.37 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,52 +2,33 @@ import java.util.*;
 import java.util.stream.Collectors;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-         Map<String, Integer> map = new HashMap<>();
+       Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < genres.length; i++) {
             map.put(genres[i], map.getOrDefault(genres[i],0) + plays[i]);
         }
-        Map<String, Map<Integer,Integer>> music = new HashMap<>();
-        for (int i = 0; i < genres.length; i++) {
-            Map<Integer, Integer> temp = music.getOrDefault(genres[i], new HashMap<>());
-            temp.put(i, plays[i]);
-            music.put(genres[i], temp);
-        }
-
-        List<String> keySet1= new ArrayList(map.keySet());
-        Collections.sort(keySet1, (s1, s2) -> map.get(s2) - (map.get(s1)));
+        //map 정렬
+        List<String> keySet = new ArrayList<>(map.keySet());
+        keySet.sort((o1,o2)-> map.get(o2) - map.get(o1));
 
         List<Integer> answer = new ArrayList<>();
-        int cnt = 0;
-        String tempCategory = "";
-        for (String category : keySet1) {
-            Map<Integer, Integer> musics = music.get(category);
-            List<Integer> genreKey = new ArrayList(musics.keySet());
-            Collections.sort(genreKey, (s1, s2) -> musics.get(s2) - (musics.get(s1)));
-
-            int idx = 0;
-            for (Integer i: genreKey) {
-                if(tempCategory.equals("")) {
-                    tempCategory = category;
-                }                
-                if(cnt < 2) {
-                    answer.add(i);
-                    cnt++;
-                    /** 해당 장르의 음악이 2개 이하인 경우 */
-                    if(idx == genreKey.size()-1) {
-                        cnt =0 ;
-                        tempCategory="";
-                    }
-                    idx++;
-
-                }
-                if(cnt==2) {
-                    cnt =0 ;
-                    tempCategory="";
-                    break;
+        for (String key : keySet) {
+            Map<Integer, Integer> x = new HashMap<>();
+            for (int i = 0; i < genres.length; i++) {
+                if(key.equals(genres[i])) {
+                    x.put(i, plays[i]);
                 }
             }
-        }
+            List<Integer> k = new ArrayList<>(x.keySet());
+            k.sort((o1,o2)->x.get(o2)-x.get(o1));
 
-        return answer.stream().mapToInt(i -> i).toArray();
+            int cnt=0;
+            for (Integer s : k) {
+                if(cnt<2) {
+                    answer.add(s);
+                    cnt++;
+                } else break;
+            }
+        }
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
