174f09cc61d124146fbea4262df7ce19dd13e744
seungeun90 <94624666+seungeun90@users.noreply.github.com>
2025-02-03 19:07:28 +0900

    [level 1] Title: 폰켓몬, Time: 2864.36 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,11 +2,28 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 class Solution {
-    public int solution(int[] nums) {
-       int max = nums.length/2;
-        Integer[] integers = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        Set<Integer> set = new HashSet<>(Arrays.asList(integers));
-        
-        return set.size() > max ? max : set.size();
+     public int solution(int[] nums) {
+        int left = 0 ;
+        int len = nums.length;
+        int target = len / 2;
+        Set<Integer> set = new HashSet<>();
+        int max = 0;
+        while(left<len-1){
+            int start = nums[left];
+            set.add(start);
+
+            for (int i = left+1; i < len; i++) {
+
+                if(set.size()>=target) {
+                    break;
+                }
+                set.add(nums[i]);
+            }
+            max = Math.max(max, set.size());
+            set = new HashSet<>();
+             left++;
+        }
+        return max;
     }
+    
 }
