baa84b4a48008fbdb509d67d8304f97a1fc7a6a3
seungeun90 <94624666+seungeun90@users.noreply.github.com>
2025-02-03 19:32:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.89 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,19 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-          String answer = "";
-        Map<String, Integer>  map = new HashMap<>();
-        for (String s : participant) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
+             Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i],0)+1);
         }
-        for (String c : completion) {
-            map.put(c, map.get(c) - 1);
+        for (int i = 0; i < completion.length; i++) {
+            map.compute(completion[i], (k,v) -> v-1);
         }
         Set<String> keySet = map.keySet();
-        for (String key: keySet) {
-            if(map.get(key)>0) {
-                answer =  key;
-                break;
-            };
-
+        for (String key : keySet) {
+            if(map.get(key)!=0) {
+                return key;
+            }
         }
-        return answer;
+        return null;
     }
 }
