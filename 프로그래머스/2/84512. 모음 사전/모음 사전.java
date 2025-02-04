8f889f5d95be8fd6690144c3deab6b116af9899d
seungeun90 <94624666+seungeun90@users.noreply.github.com>
2025-02-04 15:30:48 +0900

    [level 2] Title: 모음 사전, Time: 14.91 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,26 +2,25 @@
 import java.util.ArrayList;
 import java.util.List;
 class Solution {
-     char[] array = { 'A', 'E', 'I', 'O', 'U'};
-    List<String> list = new ArrayList<>();
-     public int solution(String word) {
-        dfs("");
+    String [] words = {"A","E","I","O","U"};
+    int cnt=0;
+    public int solution(String word) {        
+        return dfs("",word);
+    }
 
-        for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).equals(word)) {
-                return i;
+    public int dfs(String str, String target){
+        if(str.length()>5) return 0;
+        if(!str.isEmpty()) {
+            cnt++;
+            if(str.equals(target)) {
+                
+                return cnt;
             }
         }
+        for (String c : words) {
+             int result = dfs(str + c, target);
+            if (result > 0) return result;  // 목표 단어 찾으면 즉시 반환
+        }
         return 0;
-    }
-    
-    public void dfs(String str){
-        list.add(str);
-        if(str.length() >= 5) {
-            return;
-        }
-        for (int i = 0; i < array.length; i++) {
-            dfs(str + array[i]);
-        }
     }
 }
